49769532c35798cfd52e0e9277cabf8893da423e
Nikita Smyrnov <nikita.smyrnov@gmail.com>
2018-11-01 10:14:24 -0500

    Refactored code for extendability

--- FF-playoff-odds.py
+++ FF-playoff-odds.py
@@ -1,64 +1,45 @@
 # model assumes that average points scored per game and standard deviation do not change across the course of the season
-# 10k simulations takes 1 minute
+# 100k simulations takes approximately 10 minutes
 import pandas as pd
 import numpy as np
 import scipy.stats as stats
 from scipy.stats import norm
 import random
 
-def handleTies(wins, means, maxVal, division):
-    ties = []
-    if division == 1:
-        for i in range(5):
-            if wins[i] == maxVal:
-                ties.append(i)
-    elif division == 2:
-         for i in range(5, 10):
-            if wins[i] == maxVal:
-                ties.append(i)
-    else:
-        for i in range(10):
-            if wins[i] == maxVal:
-                ties.append(i)
-    
-    maxMean = -1
-    maxIndex = ties[0]
-    for i in ties:
-        if maxMean < means[i]:
-            maxMean = means[i]
-            maxIndex = i
-        
-    return maxIndex
+SIMULATIONS = 10000
+TEAMS = 10
+PLAYOFF_TEAMS = 6
 
 data = pd.read_csv("gamedata.csv");
 data = data.values
 
-teamMeans = np.empty(10)
-teamStds = np.empty(10)
-teamExpectedMeans = np.empty(10)
+teamMeans = np.empty(TEAMS)
+teamStds = np.empty(TEAMS)
+teamExpectedMeans = np.empty(TEAMS)
 
-for i in range(10):
-    teamMeans[i] = data[i][5:13].mean()
-    teamStds[i] = data[i][5:13].std()
+for i in range(TEAMS):
+    teamMeans[i] = data[i][5:len(data[i])].mean()
+    teamStds[i] = data[i][5:len(data[i])].std()
 
-teamMatchups = [[ [1, 9], [6, 8], [0, 4], [5, 7], [2, 3] ],
-                [ [0, 7], [9, 6], [1, 4], [2, 5], [3, 8] ],
+teamMatchups = [[ [0, 7], [9, 6], [1, 4], [2, 5], [3, 8] ],
                 [ [0, 6], [9, 5], [1, 8], [2, 4], [3, 7] ],
                 [ [0, 3], [8, 2], [9, 4], [1, 7], [5, 6] ],
                 [ [0, 1], [9, 2], [3, 5], [4, 6], [7, 8] ],
                 [ [0, 8], [9, 7], [2, 6], [4, 5], [1, 3] ]]
-playoffs = np.zeros(10)
-firstRoundBye = np.zeros(10)
-results = np.zeros(10)
+playoffs = np.zeros(TEAMS)
+firstRoundBye = np.zeros(TEAMS)
+results = np.zeros(TEAMS)
 
-for sims in range(100000):
-    numberOfWins = [5, 5, 0, 3, 4.5, 4, 5, 1.5, 3, 4]
+start = datetime.datetime.now().replace(microsecond=0)
+
+for sims in range(SIMULATIONS):
+    numberOfWins = [5, 6, 0, 4, 5.5, 5, 6, 1.5, 3, 4]
     
-    for i in range(6):
-        for j in range(10):
+    for i in range(len(teamMatchups)):
+        for j in range(TEAMS):
             teamExpectedMeans[j] = norm.ppf(random.random(), loc = teamMeans[j], scale = teamStds[j])
             
-        for j in range(5):
+        for j in range(int(TEAMS/2)):
             teamOne = teamMatchups[i][j][0]
             teamTwo = teamMatchups[i][j][1]
             if teamExpectedMeans[teamOne] > teamExpectedMeans[teamTwo]:
@@ -69,32 +50,32 @@ for sims in range(100000):
                 numberOfWins[teamOne] += 0.5
                 numberOfWins[teamTwo] += 0.5
     
-    for i in range(10):
+    for i in range(TEAMS):
         results[i] += numberOfWins[i]
     
-    for i in range(6):
+    for i in range(PLAYOFF_TEAMS):
         if i == 0:
-            maxVal = max(numberOfWins[0:5])
-            maxIndex = handleTies(numberOfWins, teamExpectedMeans, maxVal, i + 1)
+            maxVal = max(numberOfWins[0:int(TEAMS/2)])
+            maxIndex = handleTies(numberOfWins, teamExpectedMeans, maxVal, i)
             playoffs[maxIndex] += 1
             firstRoundBye[maxIndex] += 1
             numberOfWins[maxIndex] = -1
         elif i == 1:
-            maxVal = max(numberOfWins[5:10])
-            maxIndex = handleTies(numberOfWins, teamExpectedMeans, maxVal, i + 1)
+            maxVal = max(numberOfWins[int(TEAMS/2):TEAMS])
+            maxIndex = handleTies(numberOfWins, teamExpectedMeans, maxVal, i)
             playoffs[maxIndex] += 1
             firstRoundBye[maxIndex] += 1
-            numberOfWins[maxIndex] = -1  
+            numberOfWins[maxIndex] = -1
         else:
             maxVal = max(numberOfWins)
-            maxIndex = handleTies(numberOfWins, teamExpectedMeans, maxVal, i + 1)
+            maxIndex = handleTies(numberOfWins, teamExpectedMeans, maxVal, i)
             playoffs[maxIndex] += 1
             numberOfWins[maxIndex] = -1
 
-for i in range (10):
-    results[i] /= 100000
-    playoffs[i] /= 1000
-    firstRoundBye[i] /= 1000
+for i in range(TEAMS):
+    results[i] /= SIMULATIONS
+    playoffs[i] /= int(SIMULATIONS/TEAMS/10)
+    firstRoundBye[i] /= int(SIMULATIONS/TEAMS/10)
     print(data[i][0] + "'s projected wins: " + str(results[i]) + 
           ", projected playoff odds: " + str(playoffs[i]) + 
           ", projected odds of getting a first-round bye: " + str(firstRoundBye[i]))
